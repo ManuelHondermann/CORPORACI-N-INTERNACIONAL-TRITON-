e4479f20c568b70a5600b3003f55bcb6d1a12751
ManuelHondermann <manuelhondermannsueldo@gmail.com>
2026-02-18 09:14:27 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // ⚠️ ESTA LÍNEA ES OBLIGATORIA ⚠️
-  base: '/CORPORACI-N-INTERNACIONAL-TRITON/',
-});
